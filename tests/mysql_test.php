b2ff3a9165708282c480ff09a16fddefdae0545a
Mário <mmm.masaomukai@gmail.com>
2023-08-09 19:00:55 -0300

    abstracao

---
+++ tests/mysql_test.php
@@ -0,0 +1,33 @@
+<?php
+    require_once '../includes/funcoes.php';
+    require_once '../core/conexao_mysql.php';
+    require_once '../core/sql.php';
+    require_once '../core/mysql.php';
+
+    insert_teste('mario', 'mukai@gmail.com', 'mario123');
+    buscar_teste();
+    update_teste(38, 'murilo', 'silva@gmail.com');
+    buscar_teste();
+
+    function insert_teste($nome, $email, $senha) : void
+    {
+        $dados = ['nome' => $nome
+                , 'email' => $email
+                , 'senha' => $senha];
+        insere('usuario', $dados);
+    }
+
+    function buscar_teste() : void
+    {
+        $usuarios = buscar('usuario', ['id', 'nome', 'email'], [],'');
+        print_r($usuarios);
+    }
+
+    function update_teste($id, $nome, $email) : void
+    {
+        $dados = ['nome' => $nome
+                , 'email' => $email];
+        $criterio = [['id', '=', $id]];
+        atualiza('usuario', $dados, $criterio);
+    }
+?>
